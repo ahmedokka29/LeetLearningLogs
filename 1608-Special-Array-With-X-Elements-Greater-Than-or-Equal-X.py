ea38579a189531a1c8ba66787a241fc38f1324a4
Ahmed Gamal Okka <ahmed.okka@hotmail.com>
2025-01-02 00:02:00 +0200

    [1608] [Time Beats: 100.00%] [Memory Beats: 7.01%] - LeetPush

--- 1608-Special-Array-With-X-Elements-Greater-Than-or-Equal-X.py
+++ 1608-Special-Array-With-X-Elements-Greater-Than-or-Equal-X.py
@@ -1,15 +1,30 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        for x in range(1,len(nums)+1):
-            c = 0
-            for i in nums:
-                if i >= x:
-                    c+=1
-            if c == x: 
+        # for x in range(1, len(nums) + 1):
+        #     c = 0
+        #     for i in nums:
+        #         if i >= x:
+        #             c += 1
+        #     if c == x:
+        #         return x
+        # return -1
+
+        nums.sort()
+        N = len(nums)
+        for x in range(1, N + 1):
+            low = 0
+            high = N-1
+            index = len(nums)
+            while low<=high:
+                mid = (high + low) // 2
+                if nums[mid]>=x:
+                    index = mid
+                    high = mid - 1
+                else:
+                    low = mid+1 
+            if N - index == x:
                 return x
-        return -1 
 
 
 
-
-        
+        return -1
