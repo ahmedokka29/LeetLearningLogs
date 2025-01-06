f02c2dafc2fb31c55ee5b55027c12889597a02e7
Ahmed Gamal Okka <ahmed.okka@hotmail.com>
2025-01-06 23:51:50 +0200

    [1769] [Time Beats: 76.70%] [Memory Beats: 16.49%] - LeetPush

--- 1769-Minimum-Number-of-Operations-to-Move-All-Balls-to-Each-Box.py
+++ 1769-Minimum-Number-of-Operations-to-Move-All-Balls-to-Each-Box.py
@@ -1,12 +1,12 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         # Idea: transfer all balls to box i for each operation
-        # edge case: if one element return ans = [0] 
+        # edge case: if one element return ans = [0]
         # sudo:
         # initialize array ans of length n
-        # loop over boxes with holding the index i  
+        # loop over boxes with holding the index i
         # loop over boxes again with new index
-        # compare the index i, j 
+        # compare the index i, j
         # count operations between i , j
         # add the count to index i in ans array
         n = len(boxes)
@@ -19,48 +19,22 @@ class Solution:
         left_ops = 0
         for i in range(n):
             ans[i] += left_ops
-            if boxes[i] == '1':
+            if boxes[i] == "1":
                 left_balls += 1
             left_ops += left_balls
 
-        # 110
-        # i = 0
-        # ans[0] = 0
-        # left_balls = 1
-        # leftops = 1
-        
-        # i = 1
-        # ans[1] = 1
-        # left_balls = 2
-        # left_ops = 3
-
-        # i = 2
-        # ans[2] = 3
-        # left_balls = 2
-        # left_ops = 5
-        
         right_balls = 0
         right_ops = 0
-        for i in range(n-1, -1, -1):
+        for i in range(n - 1, -1, -1):
             ans[i] += right_ops
-            if boxes[i] == '1':
+            if boxes[i] == "1":
                 right_balls += 1
             right_ops += right_balls
-       
-        # 110
-        # i = 2
-        # ans[2] = 3
-        # right_balls = 0
-        # right_ops = 0
-        
-        # i = 1
-        # ans[1] = 1
-        # right_balls = 1
-        # right_ops = 1
 
-        # i = 0
-        # ans[0] = 1
-        # right_balls = 2
-        # right_ops = 3
+        # for i in range(n):
+        #     for j in range(n):
+        #         if i != j:
+        #             if int(boxes[j]) == 1:
+        #                 ans[i] += abs(j-i)
 
         return ans
