d74d92100c6f831890e3ac79e7df036e9caa3c8f
Ahmed Gamal Okka <ahmed.okka@hotmail.com>
2025-01-08 00:05:01 +0200

    [36] [Time Beats: 14.58%] [Memory Beats: 9.81%] - LeetPush

--- 36-Valid-Sudoku.py
+++ 36-Valid-Sudoku.py
@@ -2,13 +2,13 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
         row = 9
+        
         for x in range(row):
             numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
             for y in range(row):
                 if board[x][y] in numbers.keys():
                     numbers[board[x][y]] += 1
                     if numbers[board[x][y]] > 1:
-                        print('fuck rows')
                         return False 
 
         for y in range(row):
@@ -17,107 +17,44 @@ class Solution:
                 if board[x][y] in numbers.keys():
                     numbers[board[x][y]] += 1
                     if numbers[board[x][y]] > 1:
-                        print('fuck col')
                         return False 
 
-        # for i in range(3,row,3):
-
-        #     for x in range(i):
+        # for r in range(0,9,3):
+        #     for c in range(0,9,3):        
         #         numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-        #         for y in range(i):
-        #             print('x',x,'y',y)
-        #             # print(board[x][y])
-        #             if board[x][y] in numbers.keys():
-        #                 numbers[board[x][y]] += 1
-        #                 if numbers[board[x][y]] > 1:
-        #                     print('fuck boxes')
-        #                     return False 
-        #             # print(numbers)
-        # for i in range(2):
-        #     for x in range(i):
-        #         for y in range(x):
-        #             print(board[x][y])
+        #         for x in range(0+r,3+r):
+        #             for y in range(0+c,3+c):
+        #                 if board[x][y] in numbers.keys():
+        #                     numbers[board[x][y]] += 1
+        #                     if numbers[board[x][y]] > 1:
+        #                         return False 
+            
+        for c in range(0,9,3):        
+            numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
+            for x in range(0,3):
+                for y in range(0+c,3+c):
+                    if board[x][y] in numbers.keys():
+                        numbers[board[x][y]] += 1
+                        if numbers[board[x][y]] > 1:
+                            return False 
 
-        numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-        for x in range(0,3):
-            for y in range(0,3):
-                if board[x][y] in numbers.keys():
-                    numbers[board[x][y]] += 1
-                    if numbers[board[x][y]] > 1:
-                        print('fuck boxes 1')
-                        return False 
+        for c in range(0,9,3):        
+            numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
+            for x in range(3,6):
+                for y in range(0+c,3+c):
+                    if board[x][y] in numbers.keys():
+                        numbers[board[x][y]] += 1
+                        if numbers[board[x][y]] > 1:
+                            return False 
 
-        numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-        for x in range(0,3):
-            for y in range(3,6):
-                if board[x][y] in numbers.keys():
-                    numbers[board[x][y]] += 1
-                    if numbers[board[x][y]] > 1:
-                        print('fuck boxes 2')
-                        return False
-
-        numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-        for x in range(0,3):
-            for y in range(6,9):
-                if board[x][y] in numbers.keys():
-                    numbers[board[x][y]] += 1
-                    if numbers[board[x][y]] > 1:
-                        print('fuck boxes 3')
-                        return False 
-
-        numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-        for x in range(3,6):
-            for y in range(0,3):
-                if board[x][y] in numbers.keys():
-                    numbers[board[x][y]] += 1
-                    if numbers[board[x][y]] > 1:
-                        print('fuck boxes 4')
-                        return False 
-
-        numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-        for x in range(3,6):
-            for y in range(3,6):
-                if board[x][y] in numbers.keys():
-                    numbers[board[x][y]] += 1
-                    if numbers[board[x][y]] > 1:
-                        print('fuck boxes 5')
-                        return False 
-
-        numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-        for x in range(3,6):
-            for y in range(6,9):
-                if board[x][y] in numbers.keys():
-                    numbers[board[x][y]] += 1
-                    if numbers[board[x][y]] > 1:
-                        print('fuck boxes 6')
-                        return False 
-
-        numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-        for x in range(6,9):
-            for y in range(0,3):
-                if board[x][y] in numbers.keys():
-                    numbers[board[x][y]] += 1
-                    if numbers[board[x][y]] > 1:
-                        print('fuck boxes 7')
-                        return False 
-
-        numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-        for x in range(6,9):
-            for y in range(3,6):
-                if board[x][y] in numbers.keys():
-                    numbers[board[x][y]] += 1
-                    if numbers[board[x][y]] > 1:
-                        print('fuck boxes 8')
-                        return False 
-
-        numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
-        for x in range(6,9):
-            for y in range(6,9):
-                if board[x][y] in numbers.keys():
-                    numbers[board[x][y]] += 1
-                    if numbers[board[x][y]] > 1:
-                        print('fuck boxes 9')
-                        return False 
+        for c in range(0,9,3):        
+            numbers = {'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0,'9':0}
+            for x in range(6,9):
+                for y in range(0+c,3+c):
+                    if board[x][y] in numbers.keys():
+                        numbers[board[x][y]] += 1
+                        if numbers[board[x][y]] > 1:
+                            return False 
         
         return True
 
