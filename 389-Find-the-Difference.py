6daf02b807e135c4b96ffdc1e3ae8dabd270bc4f
Ahmed Gamal Okka <ahmed.okka@hotmail.com>
2025-01-17 23:01:40 +0200

    [389] [Time Beats: 100.00%] [Memory Beats: 23.42%] - LeetPush

--- 389-Find-the-Difference.py
+++ 389-Find-the-Difference.py
@@ -1,9 +1,12 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s = ''.join(sorted(s))
-        t = ''.join(sorted(t))
-        for i in range(len(s)):
-            if s[i]!=t[i]:
-                return t[i]
-        return t[-1]
+        # s = ''.join(sorted(s))
+        # t = ''.join(sorted(t))
+        # for i in range(len(s)):
+        #     if s[i]!=t[i]:
+        #         return t[i]
+        # return t[-1]
         
+        freq = Counter(t)
+        freq_s = Counter(s)
+        return list((freq - freq_s).keys())[0]
